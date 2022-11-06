f11815ef2f2f9d670745f27227fc77b953216e99
Keith Ma <keithchezzy@gmail.com>
2022-11-06 13:09:07 -0800

    commit tree 0ef1b305a2fa12b128bd9e459b3ee2f24328a086

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
+const { bodyParser } = require("json-server");
+
 // Add your code here
+function submitData(name,email){
+    return fetch("http://localhost:3000/users",{
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+              },
+             
+        body:JSON.stringify({
+            name,
+            email
+        })
+    })
+
+    .then(function (response) {
+    return response.json()
+    })
+    .then(function (object) {
+    document.body.innerHTML = object["id"]
+    })
+    .catch(function (requireError) {
+        document.body.innerHTML = requireError.message
+    })
+    }
